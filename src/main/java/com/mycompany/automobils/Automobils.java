5fea92bbc8977a5eb260e49abb2f6fc315113c36
Shahariar Rijon <shahariar.rijon@gmail.com>
2023-12-11 16:01:49 +0600

    login & register ui

---
+++ src/main/java/com/mycompany/automobils/Automobils.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automobils;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Automobils {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
